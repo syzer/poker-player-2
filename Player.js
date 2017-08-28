d2a0368519f9652ad177e4295c62425c90d899ba
syzer3@gmail.com <syzer3@gmail.com>
2017-08-28 19:03:03 +0200

    xo

--- Player.js
+++ Player.js
@@ -1,14 +1,14 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return '0.1'
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    bet(0)
   }
 
   static showdown(gameState) {
   }
 }
 
-module.exports = Player;
+module.exports = Player
